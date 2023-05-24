d0e7fe86b9186a928c285cff2d1ef6932e324510
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-05-24 19:50:59 +0200

    js module-2

--- js/js-tutorial.js
+++ js/js-tutorial.js
@@ -73,13 +73,13 @@ console.log(0 || ('0' && {}));
 // Вірна відповідь: {} 
 
 //* ++++++++++++++++++++++++++++++++++++++++++++
-//Що буде виведено в консоль?:
-let a = 6; 
-let b = false; 
-let c = "12"; 
-let d = 1; 
-let result = a + d + c + b; 
-console.log(result);
+// //Що буде виведено в консоль?:
+// let a = 6; 
+// let b = false; 
+// let c = "12"; 
+// let d = 1; 
+// let result = a + d + c + b; 
+// console.log(result);
 
 
 //* ++++++++++++++++++++++++++++++++++++++++++++
@@ -97,24 +97,24 @@ console.log(result);
 // }
 
 //* ++++++++++++++++++++++++++++++++++++++++++++
-const clients = ["Mango", "Poly", "Ajax"];
-const clientNameToFind = "Poly";
-let message;
+// const clients = ["Mango", "Poly", "Ajax"];
+// const clientNameToFind = "Poly";
+// let message;
 
-for (const client of clients) {
-  // На кожній ітерації будемо перевіряти чи збігається елемент масиву з
-  // іменем клієнта. Якщо збігається - записуємо в message повідомлення
-  // про успіх і робимо break, щоб далі не шукати
-  if (client === clientNameToFind) {
-    message = "Клієнт з таким ім'ям є в базі даних!";
-    break;
-  }
+// for (const client of clients) {
+//   // На кожній ітерації будемо перевіряти чи збігається елемент масиву з
+//   // іменем клієнта. Якщо збігається - записуємо в message повідомлення
+//   // про успіх і робимо break, щоб далі не шукати
+//   if (client === clientNameToFind) {
+//     message = "Клієнт з таким ім'ям є в базі даних!";
+//     break;
+//   }
 
-  // Якщо вони не збігаються - записуємо в message повідомлення про відсутність імені
-  message = "Клієнт з таким ім'ям відсутній в базі даних!";
-}
+//   // Якщо вони не збігаються - записуємо в message повідомлення про відсутність імені
+//   message = "Клієнт з таким ім'ям відсутній в базі даних!";
+// }
 
-console.log(message); // "Клієнт з таким ім'ям є в базі даних!"
+// console.log(message); // "Клієнт з таким ім'ям є в базі даних!"
 
 //* ++++++++++++++++++++++++++++++++++++++++++++
 
@@ -238,6 +238,207 @@ if (balance >= payment) {
 
 //* ++++++++++++++++++++++++++++++++++++++++++++
 
+// const clients = ["Mango", "Ajax", "Poly"];
+
+// for (let i = 0; i < clients.length; i += 1) {
+//   console.log(clients[i]);
+// }
+//* ++++++++++++++++++++++++++++++++++++++++++++
+
+// const clients = ["Mango", "Ajax", "Poly"];
+
+// for (const client of clients) {
+//   console.log(client);
+// }
+//* ++++++++++++++++++++++++++++++++++++++++++++
+
+const string = "javascript";
+
+for (const character of string) {
+  console.log(character);
+}
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+
+// const clients = ["Mango", "Poly", "Ajax"];
+// const clientNameToFind = "Poly";
+// let message;
+
+// for (const client of clients) {
+//   // На кожній ітерації будемо перевіряти чи збігається елемент масиву з
+//   // іменем клієнта. Якщо збігається - записуємо в message повідомлення
+//   // про успіх і робимо break, щоб далі не шукати
+//   if (client === clientNameToFind) {
+//     message = "Клієнт з таким ім'ям є в базі даних!";
+//     break;
+//   }
+
+//   // Якщо вони не збігаються - записуємо в message повідомлення про відсутність імені
+//   message = "Клієнт з таким ім'ям відсутній в базі даних!";
+// }
+
+// console.log(message); // "Клієнт з таким ім'ям є в базі даних!"
+//* ++++++++++++++++++++++++++++++++++++++++++++
+
+// const clients = ["Mango", "Poly", "Ajax"];
+// const clientNameToFind = "Poly";
+// let message = "Клієнт з таким ім'ям відсутній в базі даних!";
+
+// for (const client of clients) {
+//   if (client === clientNameToFind) {
+//     message = "Клієнт з таким ім'ям є в базі даних!";
+//     break;
+//   }
+//   // Якщо не збігається, то на цій ітерації нічого не робимо
+// }
+
+// console.log(message); // Клієнт з таким ім'ям є в базі даних!
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+// const numbers = [1, 3, 14, 18, 4, 7, 29, 6, 34];
+// const threshold = 15;
+
+// // Для чисел, менших ніж порогове значення, спрацьовує continue, виконання тіла
+// // припиняється і управління передається на наступну ітерацію.
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] < threshold) {
+//     continue;
+//   }
+
+//   console.log(`Число більше за ${threshold}: ${numbers[i]}`); // 18, 29, 34
+// }
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+
+// Push
+
+const a = ["Mango"];
+// Оскільки a - це масив, в b записується посилання на вже існуючий
+// масив в пам'яті. Тепер a і b вказують на той самий масив.
+const b = a;
+console.log(a); // ["Mango"]
+console.log(b); // ["Mango"]
+
+// Змінимо масив, додавши ще один елемент, використовуючи вказівник з a
+a.push("Poly");
+console.log(a); // ["Mango", "Poly"]
+
+// b також змінилось, тому що b, як і a,
+// просто містить посилання на те ж саме місце в пам'яті
+console.log(b); // ["Mango", "Poly"]
+
+// Результат повторюється
+b.push("Ajax");
+console.log(a); // ["Mango", "Poly", "Ajax"]
+console.log(b); // ["Mango", "Poly", "Ajax"]
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+// Метод split() 
+const name = "Mango";
+console.log(name.split("")); // ["M", "a", "n", "g", "o"]
+
+const message = "JavaScript - це цікаво";
+console.log(message.split(" ")); // ["JavaScript", "-", "це", "цікаво"]
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+//  Метод join()
+const words = ["JavaScript", "це", "цікаво"];
+console.log(words.join("")); // "JavaScriptцецікаво"
+console.log(words.join(" ")); // "JavaScript це цікаво"
+console.log(words.join("-")); // "JavaScript-це-цікаво"
+//* ++++++++++++++++++++++++++++++++++++++++++++
+// //  Метод indexOf()
+// const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// console.log(clients.indexOf("Poly")); // 2
+// console.log(clients.indexOf("Monkong")); // -1
+//* ++++++++++++++++++++++++++++++++++++++++++++
+// Метод includes()
+const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+console.log(clients.includes("Poly")); // true
+console.log(clients.includes("Monkong")); // false
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+// Перевірка багатьох умов з includes()
+
+// // 1.
+// const fruit = "apple";
+
+// if (fruit === "apple" || fruit === "strawberry") {
+//   console.log("It is a red fruit!");
+// }
+
+// // 2.
+// const fruit = "apple";
+
+// if (
+//   fruit === "apple" ||
+//   fruit === "strawberry" ||
+//   fruit === "cherry" ||
+//   fruit === "cranberries"
+// ) {
+//   console.log("It is a red fruit!");
+// }
+
+// 3.
+// Виносимо варіанти в масив
+const redFruits = ["apple", "strawberry", "cherry", "cranberries"];
+const fruit = "cherry";
+// Перевіряємо присутність елемента
+const hasFruit = redFruits.includes(fruit);
+
+if (hasFruit) {
+  console.log(`${fruit} is a red fruit!`);
+}
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+
+// // 1.Метод push()
+// const numbers = [];
+
+// numbers.push(1);
+// console.log(numbers); // [1]
+
+// numbers.push(2);
+// console.log(numbers); // [1, 2]
+
+// numbers.push(3);
+// console.log(numbers); // [1, 2, 3]
+
+// numbers.push(4);
+// console.log(numbers); // [1, 2, 3, 4]
+
+// numbers.push(5);
+// console.log(numbers); // [1, 2, 3, 4, 5]
+
+
+// 2.Метод pop()
+const numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers.pop()); //  5
+console.log(numbers); // [1, 2, 3, 4]
+
+console.log(numbers.pop()); //  4
+console.log(numbers); // [1, 2, 3]
+
+console.log(numbers.pop()); //  3
+console.log(numbers); // [1, 2]
+
+console.log(numbers.pop()); //  2
+console.log(numbers); // [1]
+
+console.log(numbers.pop()); //  1
+console.log(numbers); // []
+//* ++++++++++++++++++++++++++++++++++++++++++++
+
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+
 
 //* ++++++++++++++++++++++++++++++++++++++++++++
 
@@ -250,3 +451,5 @@ if (balance >= payment) {
 
 //* ++++++++++++++++++++++++++++++++++++++++++++
 
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
