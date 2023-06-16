4f13877d95542b1cf7c732ad8e2f4c6fe4a87542
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-06-16 22:30:37 +0200

    avtoperevirka mod-5

--- js/js-module5-avtoperevirka.js
+++ js/js-module5-avtoperevirka.js
@@ -41,62 +41,477 @@
 //   discount: 0.1,
 //   orders: ["Burger", "Pizza", "Salad"],
 //   // Change code below this line
-//   getBalance() {
+// getBalance(balance) {
 //     return this.balance;
 //   },
-//   getDiscount() {
+//   getDiscount(discount) {
 //     return this.discount;
 //   },
 //   setDiscount(value) {
 //     this.discount = value;
 //   },
-//   getOrders() {
+//   getOrders(order) {
 //     return this.orders;
 //   },
 //   addOrder(cost, order) {
-//     this.balance -= cost - cost * discount;
+//     this.balance -= cost - cost * this.discount;
 //     this.orders.push(order);
 //   },
 //   // Change code above this line
 // };
 
 // customer.setDiscount(0.15);
-// console.log(customer.getDiscount());
-// // 0.15
+// console.log(customer.getDiscount()); // 0.15
 // customer.addOrder(5000, "Steak");
-// console.log(customer.getBalance());
-// // 19750
+// console.log(customer.getBalance()); // 19750
 // console.log(customer.getOrders());
 // // ["Burger", "Pizza", "Salad", "Steak"]
-
-
-// // Виклик customer.getDiscount() повертає поточне значення властивості discount
-// // Виклик customer.setDiscount(0.15) оновлює значення властивості discount
-// // Виклик customer.getBalance() повертає поточне значення властивості balance.
-// // Виклик customer.getOrders() повертає поточне значення властивості orders
-// // Виклик customer.addOrder(5000, "Steak") додає "Steak" в масив значень властивості orders і оновлює баланс
-
 
 //* ++++++++++++++++++++++++++++++++++++++++++++
 // * ЗАВДАННЯ 3
 
-//* ++++++++++++++++++++++++++++++++++++++++++++
-// * ЗАВДАННЯ 4
+// const historyService = {
+//   orders: [
+//     { email: "jacob@hotmail.com", dish: "Burrito" },
+//     { email: "solomon@topmail.net", dish: "Burger" },
+//     { email: "artemis@coldmail.net", dish: "Pizza" },
+//     { email: "solomon@topmail.net", dish: "Apple pie" },
+//     { email: "jacob@hotmail.com", dish: "Taco" },
+//   ],
+//   // Change code below this line
+//   getOrdersLog() {
+//     return this.orders
+//         .map(order => `email: ${order.email} dish: ${order.dish}`)
+//         .join(" - ");
+//   },
+//   getEmails() {
+//     const emails = this.orders.map(order => order.email);
+//     const uniqueEmails = new Set(emails);
+//     return [...uniqueEmails];
+//   },
+//   getOrdersByEmail(email) {
+//     return this.orders.filter(order => order.email === email);
+//   },
+//   // Change code above this line
+// };
+
+
+// console.log(historyService.getOrdersByEmail("solomon@topmail.net"));
+// //повертає[{ email: "solomon@topmail.net", dish: "Burger" }, { email: "solomon@topmail.net", dish: "Apple pie" }]
+// console.log(historyService.getOrdersByEmail("artemis@coldmail.net"));
+// //повертає[{ email: "artemis@coldmail.net", dish: "Pizza" }]
 
 //* ++++++++++++++++++++++++++++++++++++++++++++
-// * ЗАВДАННЯ 5
+// * ЗАВДАННЯ 4 - Object.create()
+
+// const parent = {
+//   name: "Stacey",
+//   surname: "Moore",
+//   age: 54,
+//   heritage: "Irish",
+// };
+// // Change code below this line
+
+// const child = Object.create(parent);
+
+// // Change code above this line
+// child.name = "Jason";
+// child.age = 27;
+
+
+
+// console.log(parent.hasOwnProperty("surname"));
+// // повертає true
+// console.log(parent.hasOwnProperty("heritage"));
+// // повертає true
+
+// console.log(child.hasOwnProperty("name"));
+// // повертає true
+// console.log(child.name);
+// // повертає "Jason"
+
+// console.log(child.hasOwnProperty("age"));
+// // повертає true
+// console.log(child.age);
+// // повертає 27
+
+// console.log(child.hasOwnProperty("surname"));
+// // повертає false
+// console.log(child.surname);
+// // повертає "Moore"
+
+// console.log(child.hasOwnProperty("heritage"));
+// // повертає false
+// console.log(child.heritage);
+// //повертає "Irish"
+
+// console.log(parent.isPrototypeOf(child));
+// // повертає true
+
 
 //* ++++++++++++++++++++++++++++++++++++++++++++
-// * ЗАВДАННЯ 6
+// * ЗАВДАННЯ 5 - ланцюжок прототипів
+
+// const ancestor = {
+//   name: "Paul",
+//   age: 83,
+//   surname: "Dawson",
+//   heritage: "Irish",
+// };
+// // Change code below this line
+
+// const parent = Object.create(ancestor);
+// parent.name = "Stacey";
+// parent.surname = "Moore";
+// parent.age = 54;
+
+// const child = Object.create(parent);
+// child.name = "Jason";
+// child.age = 27;
+
+// // Change code above this line
+
+// console.log(ancestor.isPrototypeOf(parent));
+// // повертає true
+// console.log(parent.isPrototypeOf(child));
+// //повертає true
+// console.log(ancestor.hasOwnProperty("surname"));
+// // повертає true
+// console.log(ancestor.surname);
+// // повертає "Dawson"
+// console.log(parent.hasOwnProperty("surname"));
+// // повертає true
+// console.log(parent.surname);
+// // повертає "Moore"
+// console.log(child.hasOwnProperty("surname"));
+// // повертає false
+// console.log(child.surname);
+// // повертає "Moore"
+// console.log(ancestor.hasOwnProperty("heritage"));
+// // повертає true
+// console.log(ancestor.heritage);
+// // повертає "Irish"
+// console.log(parent.hasOwnProperty("heritage"));
+// // повертає false
+// console.log(parent.heritage);
+// //повертає "Irish"
+// console.log(child.hasOwnProperty("heritage"));
+// // повертає false
+// console.log(child.heritage);
+// // повертає "Irish"
+
 
 //* ++++++++++++++++++++++++++++++++++++++++++++
-// * ЗАВДАННЯ 7
+// * ЗАВДАННЯ 6 - Оголошення класу
+
+// class Car {}
+// console.log(new Car());
+// // {}
+ 
+//* ++++++++++++++++++++++++++++++++++++++++++++
+// * ЗАВДАННЯ 7 - Конструктор класу - constructor
+
+// class Car {
+//   // Change code below this line
+
+//     constructor(brand, model, price) { 
+//         this.brand = brand;
+//         this.model = model; 
+//         this.price = price;
+//     }
+//   // Change code above this line
+// }
+
+// console.log(new Car("Audi", "Q3", 36000));
+// // утвориться об'єкт { brand: "Audi", model: "Q3", price: 36000 }
+// console.log(new Car("BMW", "X5", 58900));
+// // утвориться об'єкт { brand: "BMW", model: "X5", price: 58900 }
+// console.log(new Car("Nissan", "Murano", 31700));
+// // утвориться об'єкт { brand: "Nissan", model: "Murano", price: 31700 }
 
 //* ++++++++++++++++++++++++++++++++++++++++++++
 // * ЗАВДАННЯ 8
 
+// class Car {
+//   // Change code below this line
+//     constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code above this line
+// }
+
+// console.log(new Car({ brand: "Audi", model: "Q3", price: 36000 }));
+// // утвориться об'єкт { brand: "Audi", model: "Q3", price: 36000 }
+// console.log(new Car({ brand: "BMW", model: "X5", price: 58900 }));
+// // утвориться об'єкт { brand: "BMW", model: "X5", price: 58900 }
+// console.log(new Car({ brand: "Nissan", model: "Murano", price: 31700 }));
+// // утвориться об'єкт { brand: "Nissan", model: "Murano", price: 31700 }
+
 //* ++++++++++++++++++++++++++++++++++++++++++++
 // * ЗАВДАННЯ 9
 
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code below this line
+//     getPrice() { 
+//         return this.price
+//     }
+
+//     changePrice(newPrice) { 
+//         this.price = newPrice;
+//     }
+//   // Change code above this line
+// }
+
 //* ++++++++++++++++++++++++++++++++++++++++++++
-// * ЗАВДАННЯ 10
+// * ЗАВДАННЯ 10 - склад - get, add, remove items
+
+// class Storage  {
+//     constructor(items) {
+//     this.items = items;
+//     }
+    
+//     getItems() { 
+//         return this.items;
+//     }
+
+//     addItem(newItem) { 
+//        return this.items.push(newItem);
+//     }
+
+//     removeItem(itemToRemove) { 
+//         return this.items.splice(this.items.indexOf(itemToRemove), 1)
+//     }
+//     //*АБО
+//     // removeItem(itemToRemove) { 
+//     //     this.items = this.items.filter(item => item !== itemToRemove);
+//     // }
+
+// }
+// // Change code above this line
+
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems());
+// // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems());
+// // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems());
+// // ["Nanitoids", "Antigravitator", "Droid"]
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+// * ЗАВДАННЯ 11 - конструктор рядків
+
+// class StringBuilder { 
+//     constructor(initialValue) {
+//     this.value = initialValue;
+//     }
+//     getValue() { 
+//     return this.value
+//     }
+//     padStart(str) { 
+//        this.value = str + this.value;
+//     }
+//     padEnd(str) {
+//         this.value = this.value + str;
+//     }
+//     padBoth(str) {
+//         this.value = str + this.value + str;
+
+//     }
+// }
+// // Change code above this line
+
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue());
+// // "."
+// builder.padStart("^");
+// console.log(builder.getValue());
+// // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue());
+// // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue());
+// // "=^.^="
+
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+// * ЗАВДАННЯ 12
+
+// class Car {
+//   // Change code below this line
+//     #brand;
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+    
+//     getBrand() { 
+//         return this.#brand;
+//     }
+    
+//     changeBrand(newBrand) { 
+//         this.#brand = newBrand;
+//         return changeBrand
+//     }
+
+//   // Change code above this line
+// }
+
+// console.log(new Car({ brand: "Audi", model: "Q3", price: 36000 }));
+// // утвориться об'єкт { model: "Q3", price: 36000 }
+// console.log(new Car({ brand: "bmw", model: "X5", price: 58900 }));
+// // утвориться об'єкт { model: "X5", price: 58900 }
+// console.log(new Car({ brand: "Nissan", model: "Murano", price: 31700 }));
+// // утвориться об'єкт { model: "Murano", price: 31700 }
+
+// console.log(changeBrand("Honda"));
+// // змінює значення приватної властивості brand на "Honda"
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+// * ЗАВДАННЯ 13
+
+// class Storage {
+//   // Change code below this line
+
+//     #items
+//   constructor(items) {
+//     this.#items = items;
+//   }
+
+//   getItems() {
+//     return this.#items;
+//   }
+
+//   addItem(newItem) {
+//     this.#items.push(newItem);
+//   }
+
+//   removeItem(itemToRemove) {
+//     this.#items = this.#items.filter(item => item !== itemToRemove);
+//   }
+// }
+// // Change code above this line
+
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems());
+// // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+// storage.addItem("Droid");
+// console.log(storage.getItems());
+// // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems());
+// // ["Нанитоиды", "Антигравитатор", "Droid"]
+
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+// * ЗАВДАННЯ 14
+
+// class StringBuilder {
+//   // Change code below this line
+//     #value
+
+//   constructor(initialValue) {
+//     this.#value = initialValue;
+//   }
+
+//   getValue() {
+//     return this.#value;
+//   }
+
+//   padEnd(str) {
+//     this.#value += str;
+//   }
+
+//   padStart(str) {
+//     this.#value = str + this.#value;
+//   }
+
+//   padBoth(str) {
+//     this.padStart(str);
+//     this.padEnd(str);
+//   }
+// }
+
+// // Change code above this line
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
+
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+// * ЗАВДАННЯ 15
+
+// class Car {
+//   // Change code below this line
+//     #brand;
+//     #model;
+//     #price;
+
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.#model = model;
+//     this.#price = price;
+//   }
+
+//   get brand() {
+//     return this.#brand;
+//   }
+
+//   set brand(newBrand) {
+//     this.#brand = newBrand;
+//   }
+
+//   get model() {
+//     return this.#model;
+//   }
+
+//   set model(newModel) {
+//     this.#model = newModel;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     this.#price = newPrice;
+//   }
+//   // Change code above this line
+// }
+
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+// * ЗАВДАННЯ 16
+
+
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+// * ЗАВДАННЯ 17
+
+
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+// * ЗАВДАННЯ 18
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+// * ЗАВДАННЯ 19
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+// * ЗАВДАННЯ 20
--- js/js-module5.js
+++ js/js-module5.js
@@ -1,10 +1,10 @@
 "use strict";
 
 
-const someString = "it`s my car!";
-const template = "car";
-console.log(!!~someString.indexOf(template))
-// замість індекса прийде true
+// const someString = "it`s my car!";
+// const template = "car";
+// console.log(!!~someString.indexOf(template))
+// // замість індекса прийде true
 
 //*_____________________________________________
 //*==============//*** THIS ***//==============*//
@@ -24,52 +24,53 @@ console.log(!!~someString.indexOf(template))
 //*_____________________________________________
 //* ++++++++++++++++++++++++++++++++++++++++++++
 
-const bookShelf = {
-  authors: ["Бернард Корнуелл", "Роберт Шеклі"],
-  getAuthors() {
-    return this.authors;
-  },
-  addAuthor(authorName) {
-    this.authors.push(authorName);
-  },
-};
+// const bookShelf = {
+//   authors: ["Бернард Корнуелл", "Роберт Шеклі"],
+//   getAuthors() {
+//     return this.authors;
+//   },
+//   addAuthor(authorName) {
+//     this.authors.push(authorName);
+//   },
+// };
 
-console.log(bookShelf.getAuthors());
-// ["Бернард Корнуелл", "Роберт Шеклі"]
-bookShelf.addAuthor("Лі Таніт");
-console.log(bookShelf.getAuthors());
-// ["Бернард Корнуелл", "Роберт Шеклі", "Лі Таніт"]
+// console.log(bookShelf.getAuthors());
+// // ["Бернард Корнуелл", "Роберт Шеклі"]
+// bookShelf.addAuthor("Лі Таніт");
+// console.log(bookShelf.getAuthors());
+// // ["Бернард Корнуелл", "Роберт Шеклі", "Лі Таніт"]
 
-bookShelf.addAuthor("Черепашка");
-console.log(bookShelf.getAuthors());
+// bookShelf.addAuthor("Черепашка");
+// console.log(bookShelf.getAuthors());
 
 //*_____________________________________________
 //* ++++++++++++++++++++++++++++++++++++++++++++
 
 
-function foo() {
-  console.log(this);
-}
+// function foo() {
+//   console.log(this);
+// }
 
-foo(); // window без "use strict" і undefined з "use strict"
+// foo();
+// window без "use strict" і undefined з "use strict"
 //*_____________________________________________
 //* ++++++++++++++++++++++++++++++++++++++++++++
 //* 
 //** this в методі об'єкта
 //*
 
-const petya = {
-  username: "Petya",
-  showThis() {
-    console.log(this);
-  },
-  showName() {
-    console.log(this.username);
-  },
-};
+// const petya = {
+//   username: "Petya",
+//   showThis() {
+//     console.log(this);
+//   },
+//   showName() {
+//     console.log(this.username);
+//   },
+// };
 
-petya.showThis(); // {username: "Petya", showThis: ƒ, showName: ƒ}
-petya.showName(); // 'Petya'
+// petya.showThis(); // {username: "Petya", showThis: ƒ, showName: ƒ}
+// petya.showName(); // 'Petya'
 
 //*_____________________________________________
 //* ++++++++++++++++++++++++++++++++++++++++++++
@@ -100,23 +101,23 @@ petya.showName(); // 'Petya'
 //* 
 // ** this у стрілочних функціях
 //*
-const hotel = {
-  username: "Resort hotel",
-  showThis() {
-    const foo = () => {
-      // Стрілки запам'ятовують контекст під час оголошення
-      // з батьківської області видимості
-      console.log("this in foo: ", this);
-    };
+// const hotel = {
+//   username: "Resort hotel",
+//   showThis() {
+//     const foo = () => {
+//       // Стрілки запам'ятовують контекст під час оголошення
+//       // з батьківської області видимості
+//       console.log("this in foo: ", this);
+//     };
 
-    foo();
-    console.log("this in showThis: ", this);
-  },
-};
+//     foo();
+//     console.log("this in showThis: ", this);
+//   },
+// };
 
-hotel.showThis();
-// this in foo: {username: 'Resort hotel', showThis: ƒ}
-// this in showThis: {username: 'Resort hotel',showThis: ƒ}
+// hotel.showThis();
+// // this in foo: {username: 'Resort hotel', showThis: ƒ}
+// // this in showThis: {username: 'Resort hotel',showThis: ƒ}
 
 
 //*_____________________________________________
@@ -148,26 +149,338 @@ hotel.showThis();
 //*_____________________________________________
 //* ++++++++++++++++++++++++++++++++++++++++++++
 //* 
-// ** Метод apply() //ппередасть елементи масиву як окремі аргумент
+// ** Метод apply() // передасть елементи масиву як окремі аргумент
 //*
 
-function greetGuest(greeting) {
-  console.log(`${greeting}, ${this.username}.`);
+// function greetGuest(greeting) {
+//   console.log(`${greeting}, ${this.username}.`);
+// }
+
+// const mango = {
+//   username: "Манго",
+// };
+// const poly = {
+//   username: "Полі",
+// };
+
+// greetGuest.apply(mango, ["Ласкаво просимо"]); // Ласкаво просимо, Манго.
+// greetGuest.apply(poly, ["З прибуттям"]); // З прибуттям, Полі.
+
+//*_____________________________________________
+//* ++++++++++++++++++++++++++++++++++++++++++++
+
+
+//*_____________________________________________
+//* ++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+// * Метод Object.create(obj)
+
+// const animal = {
+//   legs: 4,
+// };
+// const dog = Object.create(animal);
+// dog.name = "Манго";
+
+// console.log(dog); // { name: 'Манго', __proto__: animal }
+// console.log(animal.isPrototypeOf(dog)); // true
+
+// console.log(dog.hasOwnProperty("name")); // true
+// console.log(dog.name); // 'Манго'
+
+// console.log(dog.hasOwnProperty("legs")); // false
+// console.log(dog.legs); // 4
+
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+// * Метод hasOwnProperty()
+
+// const animal = { eats: true };
+// const dog = Object.create(animal);
+// dog.barks = true;
+
+// for (const key in dog) {
+//   console.log(key); // barks, eats
+// }
+//* ++++++++++++++++++++++++++++++++++++++++++++
+
+// const animal = {
+//   eats: true,
+// };
+// const dog = Object.create(animal);
+// dog.barks = true;
+
+// for (const key in dog) {
+//   if (!dog.hasOwnProperty(key)) continue;
+
+//   console.log(key); // barks
+// }
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+// const animal = {
+//   eats: true,
+// };
+// const dog = Object.create(animal);
+// dog.barks = true;
+
+// const dogKeys = Object.keys(dog);
+
+// console.log(dogKeys); // ['barks']
+
+//*_____________________________________________
+//* ++++++++++++++++++++++++++++++++++++++++++++
+//* 
+// ** Класи - Оголошення класу
+//*
+
+// class User {
+//   // Тіло класу
+// }
+
+// const mango = new User();
+// console.log(mango); // {}
+
+// const poly = new User();
+// console.log(poly); // {}
+
+
+//*_____________________________________________
+//* ++++++++++++++++++++++++++++++++++++++++++++
+//* 
+// ** Конструктор класу - constructor
+//*
+
+// class User {
+//   // Синтаксис оголошення методу класу
+//   constructor(name, email) {
+//     // Ініціалізація властивостей екземпляра
+//     this.name = name;
+//     this.email = email;
+//   }
+// }
+
+// const mango = new User("Манго", "mango@mail.com");
+// console.log(mango);
+// // { name: 'Манго', email: 'mango@mail.com' }
+
+// const poly = new User("Поли", "poly@mail.com");
+// console.log(poly);
+// // { name: 'Поли', email: 'poly@mail.com' }
+
+//*_____________________________________________
+//* ++++++++++++++++++++++++++++++++++++++++++++
+//* 
+// ** Методи класу
+//*
+
+// class User {
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.email = email;
+//   }
+
+//   // Метод getEmail
+//   getEmail() {
+//     return this.email;
+//   }
+
+//   // Метод changeEmail
+//   changeEmail(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+//*_____________________________________________
+//* ++++++++++++++++++++++++++++++++++++++++++++
+//* 
+// ** Приватні властивості
+//*
+
+// class User {
+//   // Необов'язкове оголошення публічних властивостей
+//   name;
+//   // Обов'язкове оголошення приватних властивостей
+//   #email;
+
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.#email = email;
+//   }
+
+//   getEmail() {
+//     return this.#email;
+//   }
+
+//   changeEmail(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+
+// const mango = new User({
+//   name: "Манго",
+//   email: "mango@mail.com",
+// });
+// mango.changeEmail("mango@supermail.com");
+// console.log(mango.getEmail()); // mango@supermail.com
+// console.log(mango.#email); // Виникне помилка, це приватна властивість
+
+//*_____________________________________________
+//* ++++++++++++++++++++++++++++++++++++++++++++
+//* 
+// ** Геттери і сеттери - get, set
+//*
+// class User {
+//   #email;
+
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.#email = email;
+//   }
+
+//   // Геттер email
+//   get email() {
+//     return this.#email;
+//   }
+
+//   // Сеттер email
+//   set email(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+
+// const mango = new User({ name: "Манго", email: "mango@mail.com" });
+// console.log(mango.email); // mango@mail.com
+// mango.email = "mango@supermail.com";
+// console.log(mango.email); // mango@supermail.com
+
+//* ++++++++++++++++++++++++++++++++++++++++++++
+
+// set email(newEmail) {
+//   if(newEmail === "") {
+//     console.error("Помилка! Пошта не може бути порожнім рядком!");
+//     return;
+//   }
+
+//   this.#email = newEmail;
+// }
+
+//*_____________________________________________
+//* ++++++++++++++++++++++++++++++++++++++++++++
+//* 
+// ** padStart()
+//*
+//padStart(targetLength)
+//padStart(targetLength, padString)
+
+
+// "abc".padStart(10); // "       abc"
+// "abc".padStart(10, "foo"); // "foofoofabc"
+// "abc".padStart(6, "123465"); // "123abc"
+// "abc".padStart(8, "0"); // "00000abc"
+// "abc".padStart(1); // "abc"
+
+
+// const str1 = '5';
+
+// console.log(str1.padStart(2, '0'));
+// // Expected output: "05"
+
+// const fullNumber = '2034399002125581';
+// const last4Digits = fullNumber.slice(-4);
+// const maskedNumber = last4Digits.padStart(fullNumber.length, '*');
+
+// console.log(maskedNumber);
+// // Expected output: "************5581"
+
+
+
+//*_____________________________________________
+//* ++++++++++++++++++++++++++++++++++++++++++++
+//* 
+// ** padEnd()
+//*
+
+//padEnd(targetLength)
+//padEnd(targetLength, padString)
+
+
+// const str1 = 'Breaded Mushrooms';
+
+// console.log(str1.padEnd(25, '.'));
+// // Expected output: "Breaded Mushrooms........"
+
+// const str2 = '200';
+
+// console.log(str2.padEnd(5));
+// // Expected output: "200  "
+
+//*_____________________________________________
+//* ++++++++++++++++++++++++++++++++++++++++++++
+//* 
+// ** Статичні властивості - static
+//*
+
+// class User {
+//   // Оголошення та ініціалізація статичної властивості
+//   static Roles = {
+//     ADMIN: "admin",
+//     EDITOR: "editor",
+//   };
+
+//   #email;
+//   #role;
+
+//   constructor({ email, role }) {
+//     this.#email = email;
+//     this.#role = role;
+//   }
+
+//   get role() {
+//     return this.#role;
+//   }
+
+//   set role(newRole) {
+//     this.#role = newRole;
+//   }
+// }
+
+// const mango = new User({
+//   email: "mango@mail.com",
+//   role: User.Roles.ADMIN,
+// });
+
+// console.log(mango.Roles); // undefined
+// console.log(User.Roles); // { ADMIN: "admin", EDITOR: "editor" }
+
+// console.log(mango.role); // "admin"
+// mango.role = User.Roles.EDITOR;
+// console.log(mango.role); // "editor"
+
+// //*_____________________________________________
+//* ++++++++++++++++++++++++++++++++++++++++++++
+//* 
+// ** Статичні методи
+//*
+
+class User {
+  static #takenEmails = [];
+
+  static isEmailTaken(email) {
+    return User.#takenEmails.includes(email);
+  }
+
+  #email;
+
+  constructor({ email }) {
+    this.#email = email;
+    User.#takenEmails.push(email);
+  }
 }
 
-const mango = {
-  username: "Манго",
-};
-const poly = {
-  username: "Полі",
-};
+const mango = new User({ email: "mango@mail.com" });
 
-greetGuest.apply(mango, ["Ласкаво просимо"]); // Ласкаво просимо, Манго.
-greetGuest.apply(poly, ["З прибуттям"]); // З прибуттям, Полі.
-
-//*_____________________________________________
-//* ++++++++++++++++++++++++++++++++++++++++++++
-
-
-//*_____________________________________________
-//* ++++++++++++++++++++++++++++++++++++++++++++
+console.log(User.isEmailTaken("poly@mail.com"));
+console.log(User.isEmailTaken("mango@mail.com"));
