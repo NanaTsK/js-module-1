ce2a7089846308bd100b2fee7ea319dc0d0a0bcd
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-06-17 20:53:36 +0200

    avtoperevirka mod-5

--- js/js-module5-avtoperevirka.js
+++ js/js-module5-avtoperevirka.js
@@ -603,43 +603,6 @@
 //* ++++++++++++++++++++++++++++++++++++++++++++
 // * ЗАВДАННЯ 19
 
-class User {
-  email;
-
-  constructor(email) {
-    this.email = email;
-  }
-
-  get email() {
-    return this.email;
-  }
-
-  set email(newEmail) {
-    this.email = newEmail;
-  }
-}
-
-class Admin extends User {
-  // Change code below this line
-
-  static AccessLevel = {
-    BASIC: "basic",
-    SUPERUSER: "superuser",
-  };
-
-  // Change code above this line
-}
-
-const mango = new Admin({
-  email: "mango@mail.com",
-  accessLevel: Admin.AccessLevel.SUPERUSER,
-});
-
-console.log(mango.email);
-// "mango@mail.com"
-console.log(mango.accessLevel);
-// "superuser"
-
 
 //* ++++++++++++++++++++++++++++++++++++++++++++
 // * ЗАВДАННЯ 20
